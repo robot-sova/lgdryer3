ec32d1fe841d47db36a0bdae7211531ea5995a9a
Roman <roman@lgdryer.repair>
2026-03-26 18:15:48 -0700

    fix: remove cloudflare adapter, fix wrangler config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from '@astrojs/cloudflare';
+// import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://lgdryer.repair',
   integrations: [sitemap()],
-  adapter: cloudflare(),
+  // adapter: cloudflare(),
 });
