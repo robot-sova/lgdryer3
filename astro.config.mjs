f0327b3738d92bb2633a2ccf8521b49f2550771d
Roman <roman@lgdryer.repair>
2026-03-26 18:18:00 -0700

    fix: explicit static output, remove cloudflare adapter completely

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-// import cloudflare from '@astrojs/cloudflare';
-
 export default defineConfig({
   site: 'https://lgdryer.repair',
+  output: 'static',
   integrations: [sitemap()],
-  // adapter: cloudflare(),
 });
