54c05f8dc82d62c82b19bb23b6b168c997a1d115
Roman <roman@lgdryer.repair>
2026-03-26 17:36:40 -0700

    Add Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+import cloudflare from '@astrojs/cloudflare';
+
+export default defineConfig({
+  site: 'https://lgdryer.repair',
+  integrations: [sitemap()],
+  adapter: cloudflare(),
+});
